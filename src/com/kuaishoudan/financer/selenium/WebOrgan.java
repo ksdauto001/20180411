098fc15930aa544be9b864f45f0ceb6a86a5796b
Administrator <liuhl0@126.com>
2018-05-08 11:41:27 +0800

    050802

--- src/com/kuaishoudan/financer/selenium/WebOrgan.java
+++ src/com/kuaishoudan/financer/selenium/WebOrgan.java
@@ -18,7 +18,6 @@ import com.kuaishoudan.financer.util.RandomValue;
 
 public class WebOrgan {
 
-
 	/**
 	 * 供应商机构管理
 	 * 
@@ -26,11 +25,12 @@ public class WebOrgan {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		try {
 			WebDriver driver = WebUtil.getdriver();
 			KSDCase ksd = RandomValue.getRandom();
-			getImge3(driver,ksd);
+			List<Integer> list = getImge1(driver, ksd);
+
 			Thread.sleep(25000);
 			driver.quit();
 		} catch (MalformedURLException e) {
@@ -39,12 +39,12 @@ public class WebOrgan {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-			
+		}
+
 	}
 
-	public  static void test1(	WebDriver driver,KSDCase ksd ) throws MalformedURLException, InterruptedException {
-
+	public static void test1(WebDriver driver, KSDCase ksd)
+			throws MalformedURLException, InterruptedException {
 
 		ksd.setProduct("李氏产品-那家店");// qita22-其他22产品1
 		WebUtil.login(driver, ksd.getLoginemail());// 登录
@@ -56,9 +56,9 @@ public class WebOrgan {
 
 		// List<WebElement> mask
 		// =driver.findElements(By.className("org_mask"));//org_mask
-		List<WebElement> pages=	driver.findElements(
-				By.xpath("//ul[@class='page_list']/li"));
-		loop: for (int j = 0; j <(pages.size()-4); j++) {
+		List<WebElement> pages = driver.findElements(By
+				.xpath("//ul[@class='page_list']/li"));
+		loop: for (int j = 0; j < (pages.size() - 4); j++) {
 
 			List<WebElement> ws = driver.findElements(By.className("org_name"));
 			System.out.println(ws.size());
@@ -89,213 +89,206 @@ public class WebOrgan {
 					.click();
 		}
 		Thread.sleep(1000);
-		//testQdzl(driver);
+		// testQdzl(driver);
 
-		/*Thread.sleep(25000);
-		driver.quit();*/
+		/*
+		 * Thread.sleep(25000); driver.quit();
+		 */
 
 	}
-	public static void testJjzl(WebDriver driver) throws InterruptedException{
+
+	public static List<Integer> testJjzl(WebDriver driver)
+			throws InterruptedException {
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
 
 		WebElement titos = driver.findElement(By
-				.xpath("//ul[@class='tab_list inline_block']/li[2]"));//进件资料
+				.xpath("//ul[@class='tab_list inline_block']/li[2]"));// 进件资料
 		titos.click();
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
-		driver.findElement(By.id("editOrg_jump")).click();//编辑
+		driver.findElement(By.id("editOrg_jump")).click();// 编辑
 		// swipeTodown(driver);
 		driver.manage().timeouts().implicitlyWait(23, TimeUnit.SECONDS);
-	
-	/*	List<WebElement> checkalls=		driver.findElements(By.className("check_all")) ;//全选
-		checkalls.get(3).click();*/
 
-		List<Integer> list1=	RandomValue.getImg1();
-		List<WebElement> ws =driver.findElements(By.tagName("label")) ;
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			String classes=ws.get(k).getAttribute("class");		
-					if((labels!=null)){
-						if(classes.equals("checked")){
-				
-							ws.get(k).click();
-						}		
-						
-					}			
-		}
-		Thread.sleep(500);
-		for(int i=0;i<6;i++){
-		driver.findElements(By.className("must_nosend")).get(i).click();//非必填
-		}
-		Thread.sleep(500);
-		for(Integer musttype:list1){
-			if(musttype<9){
-				driver.findElements(By.className("must_send")).get(musttype).click();//必填
+		/*
+		 * List<WebElement> checkalls=
+		 * driver.findElements(By.className("check_all")) ;//全选
+		 * checkalls.get(3).click();
+		 */
+
+		List<Integer> list1 = RandomValue.getImg1();
+		List<WebElement> ws = driver.findElements(By.tagName("label"));
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			String classes = ws.get(k).getAttribute("class");
+			if ((labels != null)) {
+				if (classes.equals("checked")) {
+
+					ws.get(k).click();
+				}
+
 			}
 		}
-	
-	//	driver.findElements(By.className("check_all")).get(11).click();
-		Thread.sleep(1000);
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			//System.out.println(labels);
+		Thread.sleep(500);
+		for (int i = 0; i < 6; i++) {
+			driver.findElements(By.className("must_nosend")).get(i).click();// 非必填
+		}
+		Thread.sleep(500);
+		for (Integer musttype : list1) {
+			if (musttype < 9) {
+				driver.findElements(By.className("must_send")).get(musttype)
+						.click();// 必填
+			}
+		}
 
-			for(int imgtype:list1){
-				
-			 	if((labels!=null)&&labels.equals(""+imgtype)){
-					ws.get(k).click();//选项
-					
+		// driver.findElements(By.className("check_all")).get(11).click();
+		Thread.sleep(1000);
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			// System.out.println(labels);
+
+			for (Integer imgtype : list1) {
+
+				if ((labels != null) && labels.equals("" + imgtype)) {
+					ws.get(k).click();// 选项
+
 				}
 			}
 		}
 		Thread.sleep(1000);
-		/*for(int musttype:list1){
-			if(musttype>9&&musttype<19){
-			  switch(musttype){
-		        case 10:
-		       	 System.out.println("default10");
-		            driver.findElements(By.className("check_all")).get(1).click();
-		    		Thread.sleep(2000);break;
-		        case 11:
-		        		 System.out.println("default11");
-		        	 driver.findElements(By.className("check_all")).get(3).click();
-		     		Thread.sleep(2000);break;
-		        case 12:
-		       	 System.out.println("default12");
-		        	 driver.findElements(By.className("check_all")).get(5).click();
-		        	 Thread.sleep(2000);break;
-		        case 13:
-		       	 System.out.println("default13");
-		        	driver.findElements(By.className("check_all")).get(7).click();
-		    		Thread.sleep(2000);break;
-		        case 14:
-		       	 System.out.println("default14");
-		        	driver.findElements(By.className("check_all")).get(9).click();
-		    		Thread.sleep(2000);break;
-		        case 15:
-		        	driver.findElements(By.className("check_all")).get(11).click();
-		        	 System.out.println("default15");
-		     		Thread.sleep(2000);break;
-		        default:
-		            System.out.println("default");break;
-		        }
-			}
-		}*/
-		driver.findElement(By.linkText("保存")).click();//保存
+
+		driver.findElement(By.linkText("保存")).click();// 保存
 		Thread.sleep(2000);
-		driver.findElement(By.linkText("确定")).click();//确定
-		
+		driver.findElement(By.linkText("确定")).click();// 确定
+
+		return list1;
+
 	}
-	public static void testQkzl(WebDriver driver) throws InterruptedException{
+
+	public static List<Integer> testQkzl(WebDriver driver)
+			throws InterruptedException {
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
 
 		WebElement titos = driver.findElement(By
-				.xpath("//ul[@class='tab_list inline_block']/li[3]"));//请款资料
+				.xpath("//ul[@class='tab_list inline_block']/li[3]"));// 请款资料
 		titos.click();
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
-		driver.findElement(By.id("editOrg_jump")).click();//编辑
+		driver.findElement(By.id("editOrg_jump")).click();// 编辑
 		// swipeTodown(driver);
 		driver.manage().timeouts().implicitlyWait(23, TimeUnit.SECONDS);
-	
-	/*	List<WebElement> checkalls=		driver.findElements(By.className("check_all")) ;//全选
-		checkalls.get(3).click();*/
 
-		List<Integer> list1=	RandomValue.getImg2();
-		List<WebElement> ws =driver.findElements(By.tagName("label")) ;
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			String classes=ws.get(k).getAttribute("class");		
-					if((labels!=null)){
-					//	System.out.println("=@@"+labels);
-						if(classes.equals("checked")){
-				
-							ws.get(k).click();
-						}		
-						
-					}			
-		}
-		Thread.sleep(500);
-		for(int i=0;i<=6;i++){
-		driver.findElements(By.className("must_nosend")).get(i).click();//非必填
-		}
-		Thread.sleep(500);
-		for(Integer musttype:list1){
-			if(musttype<9){
-				driver.findElements(By.className("must_send")).get(musttype).click();//必填
+		/*
+		 * List<WebElement> checkalls=
+		 * driver.findElements(By.className("check_all")) ;//全选
+		 * checkalls.get(3).click();
+		 */
+
+		List<Integer> list1 = RandomValue.getImg2();
+		List<WebElement> ws = driver.findElements(By.tagName("label"));
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			String classes = ws.get(k).getAttribute("class");
+			if ((labels != null)) {
+				// System.out.println("=@@"+labels);
+				if (classes.equals("checked")) {
+
+					ws.get(k).click();
+				}
+
 			}
 		}
-		//driver.findElements(By.className("must_send")).get(1).click();//必填
+		Thread.sleep(500);
+		for (int i = 0; i <= 6; i++) {
+			driver.findElements(By.className("must_nosend")).get(i).click();// 非必填
+		}
+		Thread.sleep(500);
+		for (Integer musttype : list1) {
+			if (musttype < 9) {
+				driver.findElements(By.className("must_send")).get(musttype)
+						.click();// 必填
+			}
+		}
+		// driver.findElements(By.className("must_send")).get(1).click();//必填
 		Thread.sleep(1000);
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			//System.out.println(labels);
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			// System.out.println(labels);
 
-			for(int imgtype:list1){
-				
-			 	if((labels!=null)&&labels.equals(""+imgtype)){
-					ws.get(k).click();//选项
-					
+			for (Integer imgtype : list1) {
+
+				if ((labels != null) && labels.equals("" + imgtype)) {
+					ws.get(k).click();// 选项
+
 				}
 			}
 		}
-		driver.findElement(By.linkText("保存")).click();//保存
+		driver.findElement(By.linkText("保存")).click();// 保存
 		Thread.sleep(2000);
-		driver.findElement(By.linkText("确定")).click();//确定
-		
+		driver.findElement(By.linkText("确定")).click();// 确定
+		return list1;
+
 	}
-	public static void testQdzl(WebDriver driver) throws InterruptedException{
+
+	public static List<Integer> testQdzl(WebDriver driver)
+			throws InterruptedException {
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
 
 		WebElement titos = driver.findElement(By
-				.xpath("//ul[@class='tab_list inline_block']/li[4]"));//归档资料
+				.xpath("//ul[@class='tab_list inline_block']/li[4]"));// 归档资料
 		titos.click();
 		driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
-		driver.findElement(By.id("editOrg_jump")).click();//编辑
+		driver.findElement(By.id("editOrg_jump")).click();// 编辑
 		// swipeTodown(driver);
 		driver.manage().timeouts().implicitlyWait(23, TimeUnit.SECONDS);
-	
-	/*	List<WebElement> checkalls=		driver.findElements(By.className("check_all")) ;//全选
-		checkalls.get(3).click();*/
 
-		List<Integer> list1=	RandomValue.getImg3();
-		List<WebElement> ws =driver.findElements(By.tagName("label")) ;
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			String classes=ws.get(k).getAttribute("class");		
-					if((labels!=null)){
-						//System.out.println("=@@"+labels);
-						if(classes.equals("checked")){
-				
-							ws.get(k).click();
-						}		
-						
-					}			
+		/*
+		 * List<WebElement> checkalls=
+		 * driver.findElements(By.className("check_all")) ;//全选
+		 * checkalls.get(3).click();
+		 */
+
+		List<Integer> list1 = RandomValue.getImg3();
+		List<WebElement> ws = driver.findElements(By.tagName("label"));
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			String classes = ws.get(k).getAttribute("class");
+			if ((labels != null)) {
+				// System.out.println("=@@"+labels);
+				if (classes.equals("checked")) {
+
+					ws.get(k).click();
+				}
+
+			}
 		}
-		for(int i=0;i<6;i++){
-		driver.findElements(By.className("must_nosend")).get(i).click();//非必填
+		for (int i = 0; i < 6; i++) {
+			driver.findElements(By.className("must_nosend")).get(i).click();// 非必填
 		}
-		for(Integer musttype:list1){
-			if(musttype<9){
-				driver.findElements(By.className("must_send")).get(musttype).click();//必填
+		for (Integer musttype : list1) {
+			if (musttype < 9) {
+				driver.findElements(By.className("must_send")).get(musttype)
+						.click();// 必填
 			}
 		}
 		Thread.sleep(1000);
-		for(int k=0;k<ws.size();k++){
-			String labels=ws.get(k).getAttribute("for");
-			//System.out.println(labels);
+		for (int k = 0; k < ws.size(); k++) {
+			String labels = ws.get(k).getAttribute("for");
+			// System.out.println(labels);
 
-			for(int imgtype:list1){
-				
-			 	if((labels!=null)&&labels.equals(""+imgtype)){
-					ws.get(k).click();//选项
-					
+			for (Integer imgtype : list1) {
+
+				if ((labels != null) && labels.equals("" + imgtype)) {
+					ws.get(k).click();// 选项
+
 				}
 			}
 		}
-		driver.findElement(By.linkText("保存")).click();//保存
+		driver.findElement(By.linkText("保存")).click();// 保存
 		Thread.sleep(2000);
-		driver.findElement(By.linkText("确定")).click();//确定
-		
+		driver.findElement(By.linkText("确定")).click();// 确定
+
+		return list1;
 	}
+
 	public static void swipeTodown(WebDriver driver) {
 		driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 		((JavascriptExecutor) driver)
@@ -307,14 +300,16 @@ public class WebOrgan {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * 进件资料-上传图片
 	 */
-	public  static void  getImge1(	WebDriver driver,KSDCase ksd ){
-		 try {
-			
-			test1(driver,ksd);
-			testJjzl(driver);
+	public static List<Integer> getImge1(WebDriver driver, KSDCase ksd) {
+		List<Integer> list = null;
+		try {
+
+			test1(driver, ksd);
+			list = testJjzl(driver);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -322,15 +317,18 @@ public class WebOrgan {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return list;
 	}
+
 	/**
 	 * 请款资料-上传图片
 	 */
-	public  static void  getImge2(	WebDriver driver,KSDCase ksd ){
-		 try {
-			
-			test1(driver,ksd);
-			testQkzl(driver);
+	public static List<Integer> getImge2(WebDriver driver, KSDCase ksd) {
+		List<Integer> list = null;
+		try {
+
+			test1(driver, ksd);
+			list = testQkzl(driver);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -338,15 +336,19 @@ public class WebOrgan {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return list;
 	}
+
 	/**
 	 * 归档资料-上传图片
 	 */
-	public  static void  getImge3(	WebDriver driver,KSDCase ksd ){
-		 try {
+	public static List<Integer> getImge3(WebDriver driver, KSDCase ksd) {
+		List<Integer> list = null;
+		try {
 
-			test1(driver,ksd);
-			testQdzl(driver);
+			test1(driver, ksd);
+			list = testQdzl(driver);
+			return list;
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -354,7 +356,8 @@ public class WebOrgan {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+		return list;
 
+	}
 
 }
